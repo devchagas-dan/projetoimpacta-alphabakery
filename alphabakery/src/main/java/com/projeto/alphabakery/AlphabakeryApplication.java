3f6de758771bdfb09323e617514e5a080dd5686e
devchagas-dan <dannyella.cardoso@hotmail.com>
2026-02-21 23:58:18 -0300

    adicionando as figuras dos produtos

---
+++ alphabakery/src/main/java/com/projeto/alphabakery/AlphabakeryApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.alphabakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphabakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphabakeryApplication.class, args);
+	}
+
+}
---
+++ alphabakery/src/test/java/com/projeto/alphabakery/AlphabakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.alphabakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphabakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
