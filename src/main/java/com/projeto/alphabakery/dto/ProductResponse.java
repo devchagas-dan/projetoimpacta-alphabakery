88f197e6f4472c9955322154bce3f3fe97f631ea
devchagas-dan <dannyella.cardoso@hotmail.com>
2026-03-08 15:54:39 -0300

    dto and repository classes were created

---
+++ src/main/java/com/projeto/alphabakery/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package com.projeto.alphabakery.dto;
+
+import com.projeto.alphabakery.entity.Brand;
+import com.projeto.alphabakery.entity.ProductType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponse {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private Long price;
+    private String pictureUrl;
+    private Brand brand;
+    private ProductType productType;
+}
---
+++ src/main/java/com/projeto/alphabakery/repository/ProductTypeRepository.java
@@ -0,0 +1,10 @@
+package com.projeto.alphabakery.repository;
+
+import com.projeto.alphabakery.entity.ProductType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductTypeRepository extends JpaRepository<ProductType, Integer> {
+
+}
